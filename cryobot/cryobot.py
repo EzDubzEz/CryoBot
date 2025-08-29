f1214d5ea8029157b3f5ed36580acb7b51fdd931
EzDubzEz <tlwtee3@gmail.com>
2025-08-28 23:33:05 -0400

    biiiiiiiiiiggggggggggg google_api changes (its finished I think (most of the changess are in the google script not actually this code (:D)))

--- cryobot/cryobot.py
+++ cryobot/cryobot.py
@@ -2,18 +2,23 @@ import discord
 from discord.ext import commands, tasks
 import aiohttp
 from helper import debugPrint, getVariable
-import datetime
+from datetime import datetime, timedelta, time
 from zoneinfo import ZoneInfo
+from scrim_classes import Scrim, Team, ScrimFormat, Reputation
+from google_api import GoogleAPI
 
 DISCORD_TOKEN = getVariable("DISCORD_TOKEN")
 CHANNEL_ID = getVariable("CHANNEL_ID")
 GUILD_ID = getVariable("GUILD_ID")
-PollSend = datetime.time(hour=6, tzinfo=ZoneInfo("America/New_York"))
 
 class CryoBot:
     def __init__(self):
         self.scrimRequests: list[discord.Message] = []
         self.scrimFounds: list[discord.Message] = []
+        self._poll_send_time = time(hour=6, tzinfo=ZoneInfo("America/New_York"))
+
+        self._google_api = GoogleAPI()
+
         intents = discord.Intents.none()
         intents.guilds = True
         intents.messages = True
@@ -23,11 +28,6 @@ class CryoBot:
         self._register_commands()
 
         self._bot.run(DISCORD_TOKEN)
-        try:
-            self.post_native_poll.start()
-            debugPrint(f"Starting repeating tracker")
-        except Exception as e:
-            debugPrint(f"Failed to start loop: {e}")
 
         
     
@@ -41,10 +41,63 @@ class CryoBot:
             except Exception as e:
                 debugPrint(f"Failed to sync commands: {e}")
             try:
-                # my_repeating_task.start()
-                debugPrint(f"Starting repeating tracker")
+                post_weekly_poll.start()
+                debugPrint(f"Starting repeating poll")
             except Exception as e:
-                debugPrint(f"Failed to start loop: {e}")
+                debugPrint(f"Failed to start poll loop: {e}")
+            try:
+                refresh_google_api.start()
+                debugPrint(f"Starting repeating Google API refresh")
+            except Exception as e:
+                debugPrint(f"Failed to start Google API refresh loop: {e}")
+        
+        @tasks.loop(time=self._poll_send_time)
+        async def post_weekly_poll():
+            now = datetime.now(ZoneInfo("America/New_York"))
+            if now.weekday() == 5:
+                start = (now + timedelta(days=2)).strftime("%d/%m")
+                end = (now + timedelta(days=8)).strftime("%d/%m")
+                HEADERS = {
+                    "Authorization": f"Bot {DISCORD_TOKEN}",
+                    "Content-Type": "application/json"
+                }
+
+                url = f"https://discord.com/api/v10/channels/{CHANNEL_ID}/messages"
+
+                payload = {
+                    "poll": {
+                        "question": {"text": f"📅 Scrim Availability {start} - {end}"},
+                        "answers": [
+                            {"poll_media": {"text": "Monday"}},
+                            {"poll_media": {"text": "Tuesday"}},
+                            {"poll_media": {"text": "Wednesday"}},
+                            {"poll_media": {"text": "Thursday"}},
+                            {"poll_media": {"text": "Friday"}},
+                            {"poll_media": {"text": "Saturday"}},
+                            {"poll_media": {"text": "Sunday"}}
+                        ],
+                        "allow_multiselect": True,
+                        "duration": 48  # in Hours 
+                    }
+                }
+
+                async with aiohttp.ClientSession() as session:
+                    async with session.post(url, headers=HEADERS, json=payload) as resp:
+                        if resp.status == 200:
+                            debugPrint("Sucessfully Posted Weekly Poll")
+                        else:
+                            debugPrint(f"Failed To Post Weekly Poll: {await resp.text()}")
+
+        @tasks.loop(hours=6)
+        async def refresh_google_api():
+            debugPrint("Refreshing Google API Token")
+            if refresh_google_api.current_loop == 0:
+                return
+            debugPrint("Refreshing Google API Token2")
+            try:
+                self._google_api.refreshCreds()
+            except Exception as e:
+                debugPrint(f"Issue in Google API loop: {e}")
 
         @self._bot.tree.command(name="hello_world2", description="Prints Hello World",  guild=discord.Object(id=GUILD_ID))
         async def hello_world2(interaction: discord.Interaction, number: str):
@@ -54,18 +107,8 @@ class CryoBot:
 
         @self._bot.tree.command(name="one", guild=discord.Object(id=GUILD_ID))
         async def one(interaction: discord.Interaction): # TODO: Make this not a command and be automatic
-            embed = discord.Embed(
-                title="Scrim Request Sent",
-                description="Cryobark has sent out a request for a scrim!",
-                color=discord.Color.gold()
-            )
-
-            embed.add_field(name="Date", value="Saturday, June 15", inline=True)
-            embed.add_field(name="Time", value="9:00 PM EST", inline=True)
-            embed.add_field(name="Format", value="3 Games", inline=True)
-            embed.set_footer(text="Awaiting opponent...")
-
-            embed.set_thumbnail(url="https://wiki.leagueoflegends.com/en-us/images/Enemy_Missing_ping.png")
+            scrim = Scrim(datetime.now(), ScrimFormat.THREE_GAMES)
+            embed = self._create_looking_for_scrim_embed(scrim)
             
             await interaction.response.send_message(embed=embed)
             self.scrimRequests.append(await interaction.original_response())
@@ -83,86 +126,98 @@ class CryoBot:
         @self._bot.tree.command(guild=discord.Object(id=GUILD_ID))
         async def three(interaction: discord.Interaction):# TODO: Make this not a command and be automatic
             await self.scrimRequests.pop(0).delete()
-            embed = discord.Embed(
-                title="Scrim Found vs Cryobarkers",
-                description="Get ready to rumble!",
-                color=discord.Color.blue(),
-                url="https://lol.gankster.gg/teams/84830/cryobark"
-            )
-
-            embed.add_field(name="Date", value="Saturday, June 15", inline=True)
-            embed.add_field(name="Time", value="9:00 PM EST", inline=True)
-            embed.add_field(name="Format", value="3 Games", inline=True)
-            
-            embed.add_field(name="Team", value="Cryobarkers", inline=True)
-            embed.add_field(name="Rank", value="Gold/Platinum", inline=True)
-            embed.add_field(name="GankRep", value="4.99", inline=True)
-
-            embed.set_author(name="OP.GG", url="https://op.gg/lol/summoners/na/TreboTehTree-NA1")
-            # embed.set_footer(text="Awaiting opponent...")
-
-            embed.set_thumbnail(url="https://wiki.leagueoflegends.com/en-us/images/On_My_Way_ping_Updated.png") #I updated this on the wiki so I could use the correct looking one :D
+            rep = Reputation("4.79")
+            team = Team("84830", "Cryobarkers", "Gold/Platinum", opggLink="https://www.op.gg/multisearch/na?summoners=DiamondPhoenix22%23NA1,Sp6rtan%23666,Superice%2300000,TreboTehTree%23NA1,BenAndM%23NA1", reputation=rep)
+            scrim = Scrim(datetime.now(), ScrimFormat.THREE_GAMES, team)
+            embed = self._create_scrim_found_embed(scrim)
             await interaction.response.send_message(content="<@&378729316990713856>", embed=embed)
             self.scrimFounds.append(await interaction.original_response())
 
         @self._bot.tree.command(guild=discord.Object(id=GUILD_ID))
         async def four(interaction: discord.Interaction):# TODO: Make this not a command and be automatic
             await self.scrimFounds.pop(0).delete()
-            embed = discord.Embed(
-                title="Scrim Canceled vs Cryobarkers",
-                description="Darn Cryobarkers how unreliable!",
-                color=discord.Color.red()
-            )
-
-            embed.add_field(name="Date", value="Saturday, June 15", inline=True)
-            embed.add_field(name="Time", value="9:00 PM EST", inline=True)
-            embed.add_field(name="Format", value="3 Games", inline=True)
-            
-            embed.add_field(name="Team", value="Cryobarkers", inline=True)
-            embed.add_field(name="Rank", value="Gold/Platinum", inline=True)
-            embed.add_field(name="GankRep", value="4.99", inline=True)
-            
-            embed.set_thumbnail(url="https://wiki.leagueoflegends.com/en-us/images/Retreat_ping.png")
+            team = Team("84830", "Cryobarkers", "Gold/Platinum", opggLink="https://www.op.gg/multisearch/na?summoners=DiamondPhoenix22%23NA1,Sp6rtan%23666,Superice%2300000,TreboTehTree%23NA1,BenAndM%23NA1")
+            scrim = Scrim(datetime.now(), ScrimFormat.THREE_GAMES, team)
+            embed = self._create_scrim_cancelled_embed(scrim)
 
             await interaction.response.send_message(content="<@&378729316990713856>", embed=embed)
 
             # embed.set_footer(text="Gankster Scrim Bot • Cryobark Ops")
             # embed.set_thumbnail(url="https://example.com/cancel_icon.png")
 
+    def _create_looking_for_scrim_embed(self, scrim: Scrim) -> discord.Embed:
+        embed = discord.Embed(
+            title="Scrim Request Sent",
+            description="Cryobark has sent out a request for a scrim!",
+            color=discord.Color.gold()
+        )
+        dateString = scrim.time.strftime("%A, %B %d")
+        timeString = scrim.time.strftime("%I:%M %p").lstrip("0")
+        embed.add_field(name="Date", value=dateString, inline=True)
+        embed.add_field(name="Time", value=timeString, inline=True)
+        embed.add_field(name="Format", value=scrim.scrimFormat.format, inline=True)
+        embed.set_footer(text="Awaiting opponent...")
+
+        embed.set_thumbnail(url="https://wiki.leagueoflegends.com/en-us/images/Enemy_Missing_ping.png")
+
+        return embed
+
+    def _create_scrim_found_embed(self, scrim: Scrim) -> discord.Embed:
+        embed = discord.Embed(
+            title=f"Scrim Found vs {scrim.team.name}",
+            description="Get ready to rumble!",
+            color=discord.Color.blue(),
+            url=f"https://lol.gankster.gg/teams/{scrim.team.number}"
+        )
+
+        dateString = scrim.time.strftime("%A, %B %d")
+        timeString = scrim.time.strftime("%I:%M %p").lstrip("0")
+        embed.add_field(name="Date", value=dateString, inline=True)
+        embed.add_field(name="Time", value=timeString, inline=True)
+        embed.add_field(name="Format", value=scrim.scrimFormat.format, inline=True)
+
+        rep = ""
+        if scrim.team.reputation:
+            rep = scrim.team.reputation.gankRep
+        embed.add_field(name="Team", value=scrim.team.name, inline=True)
+        embed.add_field(name="Rank", value=scrim.team.rank, inline=True)
+        embed.add_field(name="GankRep", value=rep, inline=True)
+
+        embed.set_author(name="OP.GG", url=scrim.team.opggLink)
+        # embed.set_footer(text="Awaiting opponent...")
+
+        embed.set_thumbnail(url="https://wiki.leagueoflegends.com/en-us/images/On_My_Way_ping.png") #I updated this on the wiki so I could use the correct looking one :D
+
+        return embed
+
+    def _create_scrim_cancelled_embed(self, scrim: Scrim) -> discord.Embed:
+        embed = discord.Embed(
+            title=f"Scrim Canceled vs {scrim.team.name}",
+            description=f"Darn {scrim.team.name} how unreliable!",
+            color=discord.Color.red()
+        )
+
+        dateString = scrim.time.strftime("%A, %B %d")
+        timeString = scrim.time.strftime("%I:%M %p").lstrip("0")
+        embed.add_field(name="Date", value=dateString, inline=True)
+        embed.add_field(name="Time", value=timeString, inline=True)
+        embed.add_field(name="Format", value=scrim.scrimFormat.format, inline=True)
+
+        # rep = ""
+        # if scrim.team.reputation:
+        #     rep = scrim.team.reputation.gankRep
+        # embed.add_field(name="Team", value=scrim.team.name, inline=True)
+        # embed.add_field(name="Rank", value=scrim.team.rank, inline=True)
+        # embed.add_field(name="GankRep", value=rep, inline=True)
+
+        embed.set_thumbnail(url="https://wiki.leagueoflegends.com/en-us/images/Retreat_ping.png")
+
+        return embed
 
 
-    @tasks.loop(time=PollSend)
-    async def post_native_poll(self):
-        time = datetime.now(ZoneInfo("America/New_York"))
-        if datetime.datetime.today().weekday() == 5:
-            HEADERS = {
-                "Authorization": f"Bot {DISCORD_TOKEN}",
-                "Content-Type": "application/json"
-            }
 
-            url = f"https://discord.com/api/v10/channels/{CHANNEL_ID}/messages"
 
-            payload = {
-                "poll": {
-                    "question": {"text": "📅 Scrim Availability"},
-                    "answers": [
-                        {"poll_media": {"text": "Monday"}},
-                        {"poll_media": {"text": "Tuesday"}},
-                        {"poll_media": {"text": "Wednesday"}},
-                        {"poll_media": {"text": "Thursday"}},
-                        {"poll_media": {"text": "Friday"}},
-                        {"poll_media": {"text": "Saturday"}},
-                        {"poll_media": {"text": "Sunday"}}
-                    ],
-                    "allow_multiselect": True,
-                    "duration": 48  # in Hours 
-                }
-            }
-
-            async with aiohttp.ClientSession() as session:
-                async with session.post(url, headers=HEADERS, json=payload) as resp:
-                    print("Status:", resp.status)
-                    print("Response:", await resp.text())
+    
 
         
 
--- cryobot/google_api.py
+++ cryobot/google_api.py
@@ -1,5 +1,5 @@
 from helper import getVariable
-from scrim_classes import Scrim, ScrimFormat, Team
+from scrim_classes import Scrim, ScrimFormat, Team, ErrorName, CryoBotError
 import requests
 from google_auth_oauthlib.flow import InstalledAppFlow
 import os
@@ -18,9 +18,6 @@ GOOGLE_URL = getVariable("GOOGLE_URL")
 class GoogleAPI:
     def __init__(self):
         self._creds = self._getCreds()
-        if self._creds != None:
-            self._headers = {"Authorization": f"Bearer {self._creds.token}","Content-Type": "application/json"}
-        pass
 
     def _getCreds(self) -> Credentials:
         creds = None
@@ -34,7 +31,7 @@ class GoogleAPI:
                 return None
             with open('google_auth_token.json', 'w') as token_file:
                 token_file.write(creds.to_json())
-
+        self._headers = {"Authorization": f"Bearer {creds.token}","Content-Type": "application/json"}
         return creds
 
     def setupCreds(self) -> None:
@@ -65,19 +62,37 @@ class GoogleAPI:
         ])
 
         self._creds = flow.run_local_server(port=8080, open_browser=False, state=state)
+        self._headers = {"Authorization": f"Bearer {self._creds.token}","Content-Type": "application/json"}
+
+        with open('google_auth_token.json', 'w') as token_file:
+            token_file.write(self._creds.to_json())
+
+    def refreshCreds(self)->None:
+        """
+        Call occasionally to just keep auth going good and creds refreshed
+
+        Returns:
+            None
+        """
+        self._creds.refresh(Request())
+        try:
+            self.update_scrim_results()
+        except:
+            pass
 
     def isAuthSetup(self) -> bool:
         return self._creds != None
 
     def _make_call(self, payload):
-        response = requests.post(GOOGLE_URL, headers=self._headers, json=payload)
+        response = requests.post(GOOGLE_URL, headers=self._headers, json=payload).json()
 
-        # TODO: Handle response.status_code
-        print(response.status_code)
-        print(response.text)
-        return response.text #res
+        if "error" in response:
+            messages = response["error"]["details"][0]["errorMessage"].removeprefix("Error: ").split(": ")
+            if messages[0] in (e.value for e in ErrorName):
+                raise CryoBotError(ErrorName(messages[0] ), messages[1])
+            raise CryoBotError(ErrorName.UNKNOWN, "", description=": ".join(messages))
 
-    def setupScrim(self, scrim: Scrim) -> None:
+    def found_scrim(self, scrim: Scrim) -> None:
         """
         Adds/Fills the team to the scouting doc, then adds a page to the Scrim Results sheet
 
@@ -89,37 +104,80 @@ class GoogleAPI:
             None
 
         Raises:
-            CryoBotError: If script fails"""
+            CryoBotError: If issue occurs"""
 
         payload = {
-            "function": "scrimFound",
-            "parameters": [scrim.team.name, f"https://lol.gankster.gg/teams/{scrim.team.number}", scrim.team.opggLink, scrim.time.strftime("%m/%d/%Y"), scrim.scrimFormat.value],
-            "devMode": True  # always run the latest code
+            "function": "foundScrim",
+            "parameters": [scrim.team.name, scrim.team.number, scrim.team.opggLink, scrim.time.strftime("%m/%d/%Y"), scrim.scrimFormat.games, scrim.scrimFormat.formatLong],
+            "devMode": True
         }
-        response = self._make_call(payload)
+        self._make_call(payload)
 
-    def updateScrimResults(self) -> None:
+    def cancel_scrim(self, scrim: Scrim) -> None:
         """
-        Updates the scrim results sheet to be called after a scrim block is played
+        Removes scrim from the scrim results sheet
 
         Args:
             scrim (Scrim): The scrim request to accept
-            accept (bool): Whether or not to accept the request
 
         Returns:
             None
 
         Raises:
-            CryoBotError: If script fails"""
+            CryoBotError: If issue occurs"""
+
+        payload = {
+            "function": "cancelScrim",
+            "parameters": [scrim.team.name, scrim.time.strftime("%m/%d/%Y")],
+            "devMode": True
+        }
+        self._make_call(payload)
+
+    def update_scrim_results(self) -> None:
+        """
+        Updates the scrim results sheet, should be called after a scrim block is played
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        Raises:
+            CryoBotError: If issue occurs"""
+
         payload = {
             "function": "updateScrimResults",
             "devMode": True
         }
-        response = self._make_call(payload)
+        self._make_call(payload)
+
+    def reset_scrim_results_data(self) -> None:
+        """
+        Reloads all of the scrim result data, this takes a while
+
+        Args:
+            None
+
+        Returns:
+            None
+
+        Raises:
+            CryoBotError: If issue occurs"""
+
+        payload = {
+            "function": "resetScrimResultsData",
+            "devMode": True
+        }
+        self._make_call(payload)
     pass
 
+
 if __name__ == "__main__":
-    team = Team("84830", "Cryoborkers", opggLink="https://op.gg/lol/summoners/na/TreboTehTree-NA1")
-    scrim = Scrim(datetime.now(), ScrimFormat.THREE_GAMES, team=team)
-    # GoogleAPI().setupScrim(scrim)
-    GoogleAPI().updateScrimResults()
+    team = Team("84830", "Cryobarkers", opggLink="https://op.gg/lol/summoners/na/TreboTehTree-NA1")
+    scrim = Scrim(datetime.now(), ScrimFormat.BEST_OF_THREE, team=team)
+    # GoogleAPI().found_scrim(scrim)
+    # GoogleAPI().updateScrimResults()
+    # GoogleAPI().setupCreds()
+    GoogleAPI().cancel_scrim(scrim)
+    # GoogleAPI().testing()

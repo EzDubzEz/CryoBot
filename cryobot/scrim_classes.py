e6756b15bbd314fb653680f088aa560cc8e9b94b
EzDubzEz <tlwtee3@gmail.com>
2025-08-28 23:33:05 -0400

    biiiiiiiiiiggggggggggg google_api changes (its finished I think (most of the changess are in the google script not actually this code (:D)))

--- cryobot/scrim_classes.py
+++ cryobot/scrim_classes.py
@@ -1,19 +1,25 @@
 from datetime import datetime
 from enum import Enum
 from dataclasses import dataclass, field
+from typing import Optional
 
 class ScrimFormat(Enum):
-    BEST_OF_ONE = "Bo1"
-    BEST_OF_TWO = "Bo2"
-    BEST_OF_THREE = "Bo3"
-    BEST_OF_FOUR = "Bo4"
-    BEST_OF_FIVE = "Bo5"
-    ONE_GAME = "1 Game"
-    TWO_GAMES = "2 Games"
-    THREE_GAMES = "3 Games"
-    FOUR_GAMES = "4 Games"
-    FIVE_GAMES = "5 Games"
-    NONE = "None"
+    BEST_OF_ONE = ("Bo1", "Best of One", 1 )
+    BEST_OF_TWO = ("Bo2", "Best of Two", 2)
+    BEST_OF_THREE = ("Bo3", "Best of Three", 3)
+    BEST_OF_FOUR = ("Bo4", "Best of Four", 4)
+    BEST_OF_FIVE = ("Bo5", "Best of Five", 5)
+    ONE_GAME = ("1 Game", "One Game", 1)
+    TWO_GAMES = ("2 Games", "Two Games", 2)
+    THREE_GAMES = ("3 Games", "Three Games", 3)
+    FOUR_GAMES = ("4 Games", "Four Games", 4)
+    FIVE_GAMES = ("5 Games", "Five Games", 5)
+    NONE = ("None", "None", 0)
+
+    def __init__(self, formatShort: str, formatLong: str, games: int):
+        self.formatShort = formatShort
+        self.formatLong = formatLong
+        self.games = games
 
 class Tier(Enum):
     IRON = "Iron"
@@ -87,18 +93,79 @@ class Scrim:
     open: bool = True
 
 class ErrorName(Enum):
-    NONE = "None",
-    MISSING_DATA = "Missing Data",
+    NONE = "None"
+    UNKNOWN = "Unknown"
+    TEAM_NAME_NULL = "TeamNameNull"
+    TEAM_NUMBER_NULL = "TeamNumberNull"
+    DATE_NULL = "DateNull"
+    FORMAT_NULL = "FormatNull"
+    INVALID_DATE = "InvalidDate"
+    NO_NEW_TEAM_X = "NoNewTeamX"
+    INVALID_CANCEL_TEAM = "InvalidCancelTeam"
+    INVALID_CANCEL_DATE = "InvalidCancelDate"
+    INVALID_CBK_PLAYERS = "InvalidCBKPlayers"
+    INVALID_CBK_CHAMPS = "InvalidCBKChamps"
+    INVALID_CBK_PICK_ORDER = "InvalidCBKPickOrder"
+    INVALID_ENEMY_CHAMPS = "InvalidEnemyChamps"
+    INVALID_ENEMY_PICK_ORDER = "InvalidEnemyPickOrder"
+    MISSING_CRYOBARK_SIDE_NAME = "MissingCryobarkSideName"
+    MISSING_ENEMY_SIDE_NAME = "MissingEnemySideName"
+    INVALID_CHAMP_NAME = "InvalidChampName"
+    INVALID_BLUE_PICK_ORDER = "InvalidBluePickOrder"
+    INVALID_RED_PICK_ORDER = "InvalidRedPickOrder"
+    MISSING_BLUE_ROUND_ONE_BANS = "MissingBlueRoundOneBans"
+    MISSING_BLUE_ROUND_TWO_BANS = "MissingBlueRoundTwoBans"
+    MISSING_RED_ROUND_ONE_BANS = "MissingRedRoundOneBans"
+    MISSING_RED_ROUND_TWO_BANS = "MissingRedRoundTwoBans"
+    ALREADY_REVIEWED_SCRIM_BLOCK = "AlreadyReviewedScrimBlock"
+    INVALID_GAME_COUNT = "InvalidGameCount"
+    INVALID_GAME_RECORD = "InvalidGameRecord"
+    INVALID_BLOCK_FORMAT = "InvalidBlockFormat"
+    MISSING_GAME_RECORD = "MissingGameRecord"
 
 ERROR_DESCRIPTIONS = {
     ErrorName.NONE: "No Issue",
-    ErrorName.MISSING_DATA: "Scrim Results has missing data, please update the empty fields then try again manually",
+    ErrorName.UNKNOWN: "No clue what went wrong",
+    ErrorName.TEAM_NAME_NULL: "Team name is missing",
+    ErrorName.TEAM_NUMBER_NULL: "Team number is missing",
+    ErrorName.DATE_NULL: "Date is missing",
+    ErrorName.FORMAT_NULL: "Format is missing",
+    ErrorName.INVALID_DATE: "Date format is invalid",
+    ErrorName.NO_NEW_TEAM_X: "No NewTeamX availible in Scouting Report, please create one",
+    ErrorName.INVALID_CANCEL_TEAM: "Team not found for the given scrim day",
+    ErrorName.INVALID_CANCEL_DATE: "No scrim page found for the given date",
+    ErrorName.INVALID_CBK_PLAYERS: "Cryobark players list is the wrong size",
+    ErrorName.INVALID_CBK_CHAMPS: "Cryobark champions list is the wrong size",
+    ErrorName.INVALID_CBK_PICK_ORDER: "Cryobark pick order list is the wrong size",
+    ErrorName.INVALID_ENEMY_CHAMPS: "Enemy champions list is the wrong size",
+    ErrorName.INVALID_ENEMY_PICK_ORDER: "Enemy pick order list is the wrong size",
+    ErrorName.MISSING_CRYOBARK_SIDE_NAME: "The side Cryobark is on is missing for the given game",
+    ErrorName.MISSING_ENEMY_SIDE_NAME: "The side the enemy team is on is missing for the given game",
+    ErrorName.INVALID_CHAMP_NAME: "Champion name is either misspelled or improperly capitalized/formatted",
+    ErrorName.INVALID_BLUE_PICK_ORDER: "Blue team pick order is wrong",
+    ErrorName.INVALID_RED_PICK_ORDER: "Red team pick order is wrong",
+    ErrorName.MISSING_BLUE_ROUND_ONE_BANS: "Blue team round one bans are missing (check completed checkbox if intended)",
+    ErrorName.MISSING_BLUE_ROUND_TWO_BANS: "Blue team round two bans are missing (check completed checkbox if intended)",
+    ErrorName.MISSING_RED_ROUND_ONE_BANS: "Red team round one bans are missing (check completed checkbox if intended)",
+    ErrorName.MISSING_RED_ROUND_TWO_BANS: "Red team round two bans are missing (check completed checkbox if intended)",
+    ErrorName.ALREADY_REVIEWED_SCRIM_BLOCK: "Scrim block has already been reviewed",
+    ErrorName.INVALID_GAME_COUNT: "Game count is invalid for the given date",
+    ErrorName.INVALID_GAME_RECORD: "Game record is unobtanable for the given format and date",
+    ErrorName.INVALID_BLOCK_FORMAT: "Scrim block format is invalid",
+    ErrorName.MISSING_GAME_RECORD: "Game record is missing for the given date",
 }
 
 @dataclass
 class CryoBotError(Exception):
-    name: str
-    description: str = field(init=False)
+    name: ErrorName
+    fields: str
+    description: Optional[str] = None
 
     def __post_init__(self):
-        self.description = ERROR_DESCRIPTIONS[self.name]
+        if self.description is None:
+            self.description = ERROR_DESCRIPTIONS[self.name]
+
+    def __str__(self):
+        if self.name == ErrorName.UNKNOWN:
+            return f"CryoBotError: ({self.description})"
+        return f"CryoBotError: ({self.name.value}: {self.description} | fields=[{self.fields}])"
